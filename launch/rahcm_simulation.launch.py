a49ccfbcf187db97127f9705adc31eccb5b08c98
Gabriel Lucena <gabriellucenasajr@gmail.com>
2025-04-03 16:29:27 -0300

    launch multiple worlds implemented

--- launch/rahcm_simulation.launch.py
+++ launch/rahcm_simulation.launch.py
@@ -1,25 +1,41 @@
 import os
+
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, SetLaunchConfiguration
-from launch.substitutions import LaunchConfiguration
-from launch.conditions import IfCondition
-from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    gazebo_model_path = '/home/gabriel/chico_ws/src/laser_rahcm_simulation/models'
+    pkg_share = FindPackageShare(package='laser_rahcm_simulation').find('laser_rahcm_simulation')
+    worlds_dir = os.path.join(pkg_share, 'worlds')
+    default_world = 'river.sdf'
     
-    world_file = os.path.join(
-        get_package_share_directory('laser_rahcm_simulation'), 'worlds', 'river.sdf'
+    world_gazebo_arg = DeclareLaunchArgument(
+        name="world", 
+        default_value=default_world, 
+        description="Name of the Gazebo world file located in the worlds directory"
+    )
+    
+    world_gazebo_path = PathJoinSubstitution([worlds_dir, LaunchConfiguration("world")])
+    
+    world_models_path = os.path.join(pkg_share, 'models')
+    install_dir = FindPackageShare(package='laser_rahcm_simulation').find('laser_rahcm_simulation')
+
+    os.environ['GAZEBO_MODEL_PATH'] = world_models_path
+    os.environ['GAZEBO_PLUGIN_PATH'] = os.environ.get('GAZEBO_PLUGIN_PATH', '') + ':' + install_dir + '/lib'
+
+    print("GAZEBO MODEL PATH==", os.environ["GAZEBO_MODEL_PATH"])
+    print("GAZEBO PLUGIN PATH==", os.environ["GAZEBO_PLUGIN_PATH"])
+
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(FindPackageShare("gazebo_ros").find("gazebo_ros"), "launch", "gazebo.launch.py")
+        ]),
+        launch_arguments={"world": world_gazebo_path}.items(),
     )
 
     return LaunchDescription([
-        SetLaunchConfiguration(
-            name='gazebo_model_path',
-            value=gazebo_model_path
-        ),
-        ExecuteProcess(
-            additional_env={'GAZEBO_MODEL_PATH': LaunchConfiguration('gazebo_model_path')},
-            cmd=['gazebo', '--verbose', world_file],
-            output='screen',
-        ),
+        world_gazebo_arg,
+        gazebo_launch,
     ])
